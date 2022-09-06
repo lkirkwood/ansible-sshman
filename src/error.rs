db2a73147fe7af36193e73bac4f47551c029bc4e
Linus Kirkwood <lkirkwood@allette.com.au>
2022-09-06 17:32:02 +1000

    added config error

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,19 @@ impl Display for UndefinedSectionError {
         write!(f, "Undefined section: {}", self.name)
     }
 }
+
+#[derive(Debug)]
+pub struct InvalidConfigError {
+    pub message: String
+}
+impl Error for InvalidConfigError {}
+impl Display for InvalidConfigError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Invalid SSH config file; {}", self.message)
+    }
+}
+impl InvalidConfigError {
+    pub fn from_str(str: &str) -> Self {
+        return InvalidConfigError { message: str.to_string() }
+    }
+}
