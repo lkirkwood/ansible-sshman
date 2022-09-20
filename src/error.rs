eba1c97abdd7fad17148848ba467053213cd6876
Linus Kirkwood <lkirkwood@allette.com.au>
2022-09-20 19:01:33 +1000

    removed inventory parsing

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,6 @@
 use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
-pub struct UndefinedGroupError {
-    pub name: String,
-}
-impl Error for UndefinedGroupError {}
-impl Display for UndefinedGroupError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Undefined section: {}", self.name)
-    }
-}
-
-#[derive(Debug)]
 pub struct InvalidConfigError {
     pub message: String,
 }
@@ -19,12 +8,5 @@ impl Error for InvalidConfigError {}
 impl Display for InvalidConfigError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "Invalid SSH config file; {}", self.message)
-    }
-}
-impl InvalidConfigError {
-    pub fn from_str(str: &str) -> Self {
-        return InvalidConfigError {
-            message: str.to_string(),
-        };
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
-mod inventory;
 mod config;
 mod error;
 
 use std::fs;
 
 use clap::Parser;
-
-use crate::inventory::SectionContainer;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -17,19 +14,13 @@ struct Args {
 
     /// Path to ssh config file.
     #[clap(short, long, value_parser)]
-    config: String
+    config: String,
 }
 
 fn main() {
     let args = Args::parse();
-    let inv_content = fs::read_to_string(args.inventory).expect("Failed to read inventory file.");
     let conf_content = fs::read_to_string(args.config).expect("Failed to read config file.");
+    let conf = config::SSHConfig::from_str(&conf_content).unwrap();
 
-    let inv = inventory::InventoryParser::inv_from_string(inv_content).unwrap();
-    let conf = config::SSHConfigParser::conf_from_string(inv, conf_content).unwrap();
-
-    for user in &conf.users {
-        println!("User {} can access section {} with hosts {:?}", 
-            user.name, user.access, conf.get_hosts(&user));
-    }
+    println!("{}", conf.playbook().unwrap());
 }
