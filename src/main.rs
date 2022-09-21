ddf254bcc1f4ef1723c8425850ca99e7404f9405
Linus Kirkwood <lkirkwood@allette.com.au>
2022-09-21 15:27:35 +1000

    removed inventory arg

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,6 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Path to ansible inventory file.
-    #[clap(short, long, value_parser)]
-    inventory: String,
-
     /// Path to ssh config file.
     #[clap(short, long, value_parser)]
     config: String,
