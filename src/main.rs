ff46bc5f68d4149d2f4a2440445c7ce0fdfc0fb8
Linus Kirkwood <lkirkwood@allette.com.au>
2022-10-04 16:56:26 +1100

    create tempfile and run playbook

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod config;
 mod error;
 mod model;
 
-use std::fs;
-
 use clap::Parser;
+use std::{fs, io::Write};
+use tempfile::NamedTempFile;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -19,5 +19,17 @@ fn main() {
     let conf_content = fs::read_to_string(args.config).expect("Failed to read config file.");
     let conf = config::SSHConfig::from_str(&conf_content).unwrap();
 
-    println!("{}", conf.playbook().unwrap());
+    let mut outfile = NamedTempFile::new().expect("Failed to create temp file.");
+    outfile
+        .write(conf.playbook().unwrap().as_bytes())
+        .expect("Failed to write to temp file.");
+
+    std::process::Command::new("ansible-playbook")
+        .arg(outfile.path().to_string_lossy().to_string())
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+
+    outfile.close().expect("Failed to remove temp file.");
 }
