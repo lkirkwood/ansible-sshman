cfc9f5b867e42b27e73e388de740552b8a3b70d7
Linus Kirkwood <linuskirkwood@gmail.com>
2023-02-17 16:56:09 +1100

    Updated to use new inv-aware methods

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod config;
 mod error;
+mod inventory;
 mod model;
 
 use clap::Parser;
@@ -12,6 +13,9 @@ struct Args {
     /// Path to ssh config file.
     #[clap(short, long, value_parser)]
     config: String,
+    /// Path to Ansible inventory file.
+    #[clap(short, long, value_parser)]
+    inventory: String,
 }
 
 fn main() {
@@ -19,9 +23,12 @@ fn main() {
     let conf_content = fs::read_to_string(args.config).expect("Failed to read config file.");
     let conf = config::SSHConfig::from_str(&conf_content).unwrap();
 
+    let inv_content = fs::read_to_string(args.inventory).expect("Failed to read inventory.");
+    let inv = inventory::InventoryParser::inv_from_string(inv_content).unwrap();
+
     let mut outfile = NamedTempFile::new().expect("Failed to create temp file.");
     outfile
-        .write(conf.playbook().unwrap().as_bytes())
+        .write(conf.apply(&inv).unwrap().as_bytes())
         .expect("Failed to write to temp file.");
 
     std::process::Command::new("ansible-playbook")
