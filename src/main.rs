3dfcb254d4d923fd8d02bcdfb39351da538e27ae
Linus Kirkwood <linuskirkwood@gmail.com>
2023-12-07 11:50:05 +1100

    Added var args etc

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ mod inventory;
 mod model;
 
 use clap::{Parser, Subcommand};
-use std::{fs, io::Write};
+use inventory::Inventory;
+use std::{fs, io::Write, process::Command};
 use tempfile::NamedTempFile;
 
 #[derive(Parser, Debug)]
@@ -13,19 +14,25 @@ struct Args {
     /// Path to ssh config file.
     #[clap(short, long, value_parser)]
     config: String,
-    /// Path to Ansible inventory file.
+
+    /// Ansible inventory. Can be any source accepted by ansible-inventory.
     #[clap(short, long, value_parser)]
     inventory: String,
 
+    /// What to do with the generated playbook.
     #[clap(subcommand)]
     command: Action,
 }
 
+/// An action to perform with a playbook.
 #[derive(Debug, Subcommand)]
-/// What to do with the generated playbook.
 enum Action {
-    /// Generates and runs the playbook immediately.
-    Run, // TODO add arbitrary args for ansible-playbook
+    /// Generates and runs the playbook immediately, with any provided arguments.
+    Run {
+        /// Extra arguments to pass to ansible-playbook.
+        #[clap(last = true)]
+        playbook_args: Vec<String>,
+    },
     /// Writes the playbook to a file.
     Write {
         /// Path to write the playbook to.
@@ -39,20 +46,37 @@ fn main() {
     let conf_content = fs::read_to_string(args.config).expect("Failed to read config file.");
     let conf = config::SSHConfig::from_str(&conf_content).unwrap();
 
-    let inv_content = fs::read_to_string(args.inventory).expect("Failed to read inventory.");
-    let inv = inventory::InventoryParser::inv_from_ini(inv_content).unwrap();
+    let inv_cmd = Command::new("ansible-inventory")
+        .args(["-i", &args.inventory])
+        .args(["--list", "--export", "--yaml"])
+        .output();
+
+    let inv_content = match inv_cmd {
+        Ok(output) => match String::from_utf8(output.stdout) {
+            Ok(content) => content,
+            Err(err) => panic!("Failed to parse inventory output as UTF-8: {err}"),
+        },
+        Err(err) => panic!("Failed to get output from inventory command: {err}"),
+    };
+
+    let inv: Inventory = match serde_yaml::from_str(&inv_content) {
+        Ok(inv) => inv,
+        Err(err) => panic!("Failed to parse inventory output yaml: {err}"),
+    };
 
     let playbook = conf.apply(&inv).unwrap();
 
     match args.command {
-        Action::Run => {
+        Action::Run { playbook_args } => {
             let mut outfile = NamedTempFile::new().expect("Failed to create temp file.");
             outfile
                 .write_all(playbook.as_bytes())
                 .expect("Failed to write to temp file.");
 
             std::process::Command::new("ansible-playbook")
+                .args(["-i", &args.inventory])
                 .arg(outfile.path().to_string_lossy().to_string())
+                .args(playbook_args)
                 .spawn()
                 .unwrap()
                 .wait()
